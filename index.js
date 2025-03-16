e46f6eae29c2f614cdc64cd28702b0c32d4df895
RunawayGOD <20022002utkarsh@gmail.com>
2025-03-16 18:46:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const RegisterModel = require('./models/register')
+
+const app = express()
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://utkarsh2002:A8075CEA@cluster0.wr9vb0t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+
+app.get("/", (req, res) => {
+    res.json("Hello world");
+})
+app.post('/register', (req, res) => {
+    const {name, email, password} = req.body;
+    RegisterModel.findOne({email: email})
+    .then(user => {
+        if(user) {
+            res.json("Already have an account")
+        } else {
+            RegisterModel.create({name: name, email: email, password: password})
+            .then(result => res.json(result))
+            .catch(err => res.json(err))
+        }
+    }).catch(err => res.json(err))
+})
+
+
+app.listen(3001, () => {
+    console.log("Server is Running")
+})
